04bc098d32de83163b657dacda2eb3355a1e108e
abbymark <abbymark@naver.com>
2021-12-27 22:24:27 +0900

    q_3

--- leetcode/medium/q_3.py
+++ leetcode/medium/q_3.py
@@ -5,21 +5,22 @@ def lengthOfLongestSubstring(s: str) -> int:
     cur_length = 0
 
     for i, letter in enumerate(s):
-        # breakpoint()
-        if letter not in dic:
+        if letter in dic and dic[letter] >= start:
+            start = dic[letter] + 1
+            cur_length = i+1 - start
             dic[letter] = i
-            cur_length += 1
-            if max_length < cur_length:
+            if cur_length > max_length:
                 max_length = cur_length
         else:
             dic[letter] = i
-            start = i + 1
-            if max_length <i+1-start:
-                max_length=i+1-start
-            cur_length = 0
-        
+            cur_length += 1
+            if cur_length > max_length:
+                max_length = cur_length
+    
     return max_length
 
+print(lengthOfLongestSubstring("tmmzuxt"))
+print(lengthOfLongestSubstring("abba"))
 print(lengthOfLongestSubstring("abcabcbb"))
 print(lengthOfLongestSubstring("a"))
 # print(lengthOfLongestSubstring(" "))
